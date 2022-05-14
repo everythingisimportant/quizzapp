2b2fc6ec6ccb2e1a5195e0438f3e418cb4ac59db
nmnhat <nmnhat@tma.com.vn>
2022-05-14 11:19:29 +0700

    add more selection for users: categories, amount, difficult level

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
+const url =
+  "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple";
 const checkmark = "<span class='checkmark'>&#x2713;</span>";
 const btn = document.getElementById("submit");
 let answerGroup = document.getElementsByClassName("answers");
 let data = [];
 let alphabet = ["A", "B", "C", "D"];
+const categories = ['Any', 'General Knowledge', 'Books', 'Film', 'Music', 'Musicals & Theatres', 'Television', 'Video Games', 'Board Games', 'Science & Nature', 'Science: Computers', 'Science: Mathematics', 'Mythology', 'Sports', 'Geography', 'History', 'Politics', 'Art', 'Celebrities', 'Animals', 'Vehicles', 'Comics', 'Science: Gadgets', 'Japanese Anime & Manga', 'Cartoon & Animations'];
 
-window.onload = () => {
+const form = document.getElementsByTagName('form')[0];
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let formData = new FormData(e.target);
+  formData = Object.fromEntries(formData);
+  fetchData(formData.category, formData.amount, formData.level);
+});
+
+const fetchData = (category, amount, level) => {
+  category = categories.indexOf(category) + 9
   fetch(
-    "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple"
+    `https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${level}&type=multiple`
   )
     .then((res) => res.json()) // convert
     .then((res) => {
       data = res.results;
       let wrap = document.querySelector("div.questions");
+      wrap.innerHTML = '';
       data.forEach((d, i) => {
         let q = d.question;
         let idxCorrectAns = Math.round(Math.random() * 3);
@@ -20,21 +34,21 @@ window.onload = () => {
         //   d.correct_answer,
         //   ...d.incorrect_answers.slice(idxCorrectAns),
         // ];
-        let ans = d.incorrect_answers
+        let ans = d.incorrect_answers;
         ans.splice(idxCorrectAns, 0, d.correct_answer);
         wrap.innerHTML += `<div class="question"><p>${i + 1}. ${q}</p></div>
-        <div class="answers">
-          ${ans
-            .map(
-              (a, i) =>
-                `<div class="answer">
-                  <span>${alphabet[i]}</span><p data-is-correct=${
-                  i == idxCorrectAns ? "true" : "false"
-                }>${a}</p>
-              </div>`
-            )
-            .join("\n")}
-        </div>`;
+      <div class="answers">
+        ${ans
+          .map(
+            (a, i) =>
+              `<div class="answer">
+                <span>${alphabet[i]}</span><p data-is-correct=${
+                i == idxCorrectAns ? "true" : "false"
+              }>${a}</p>
+            </div>`
+          )
+          .join("\n")}
+      </div>`;
       });
 
       Array.from(answerGroup).forEach((answer4) => {
@@ -63,8 +77,12 @@ window.onload = () => {
             `${
               Array.from(document.querySelectorAll("span.checkmark"))
                 .map(
-                  a => a.previousElementSibling.getAttribute("data-is-correct") === "true"
-                ).filter(Boolean).length} / ${answerGroup.length}`
+                  (a) =>
+                    a.previousElementSibling.getAttribute("data-is-correct") ===
+                    "true"
+                )
+                .filter(Boolean).length
+            } / ${answerGroup.length}`
           );
         } else {
           alert("Please choose an answer!");
@@ -73,8 +91,37 @@ window.onload = () => {
     });
 };
 
-// dom question
+window.onload = () => {
+  document.querySelector("input[type=range]").value = 1
+};
 
-// dom answers
-// a b c d
-// correct_answer incorrect_answers -> [0,1,2]
+// New data API - designed by myself
+// const dataURL = "https://627e155c271f386ceff0efb8.mockapi.io/api/quiz/question";
+
+// async function loadData() {
+//   try {
+//     const response = await fetch(dataURL);
+//     let data = await response.json();
+//     let wrap = document.querySelector("div.questions");
+//     data.map((d) => console.log(d.question));
+//     data.map((d, i) => {
+//       wrap.innerHTML += `<div class="question"><p>${i + 1}. ${
+//         d.question
+//       }</p></div>
+//       <div class="answers">
+//         ${d.answers
+//           .map(
+//             (a, i) =>
+//               `<div class="answer">
+//                 <span>${alphabet[i]}</span><p data-is-correct="${
+//                 d.correct == i + 1 ? "true" : "false"
+//               }">${a}</p>
+//             </div>`
+//           )
+//           .join("\n")}
+//       </div>`;
+//     });
+//   } catch (e) {
+//     console.log(e);
+//   }
+// }
